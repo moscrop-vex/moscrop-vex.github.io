f84cc1db3cfbb1f04773c307ff5cfad96dcb95b4
Errortoast <daniel.vanmaarseveen@gmail.com>
2026-03-09 15:12:28 -0700

    added blur under sidebar

--- main.js
+++ main.js
@@ -1,14 +1,27 @@
 function toggleMenu(){
-    document.getElementById("sideMenu").classList.toggle("open");
+    document.querySelector(".sideMenuBlur").classList.toggle("open");
 }
 
 function handleResize() {
-    const sideMenu = document.getElementById("sideMenu");
+    const sideMenuBlur = document.querySelector(".sideMenuBlur");
     const width = window.innerWidth;
 
     if (width > 900) {
-        sideMenu.classList.remove("open");
+        sideMenuBlur.classList.remove("open");
     }
 }
+
+const header = document.querySelector('.header');
+const sideMenuBlur = document.querySelector('.sideMenuBlur');
+
+function updateSideMenuPosition() {
+    const headerHeight = header.offsetHeight;
+
+    sideMenuBlur.style.top = `${headerHeight + 22}px`;
+    sideMenuBlur.style.height = `calc(100% - ${headerHeight + 42}px)`;
+}
+
+window.addEventListener('resize', updateSideMenuPosition);
+updateSideMenuPosition();
 
 window.addEventListener("resize", handleResize);
